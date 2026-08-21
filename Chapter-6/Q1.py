208c95b320e4c2f40b359ac82dd72e65b9bd6443
Subrata De <167060388+Subrata-83@users.noreply.github.com>
2026-08-21 23:27:03 +0530

    Correct logic for finding greatest of four numbers
    Fixed the logic to find the greatest of four numbers and added a comment.

--- Chapter-6/Q1.py
+++ Chapter-6/Q1.py
@@ -1,3 +1,5 @@
+# Write a program to find the greatest of four numbers entered by the user. 
+
 n1=int(input("Enter first number: "))
 n2=int(input("Enter second number: "))
 n3=int(input("Enter third number: "))
